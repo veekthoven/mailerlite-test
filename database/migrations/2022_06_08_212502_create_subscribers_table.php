6b7268934677403593bb7b14aa5195558b435f35
Victor Abbah Nkoms <veekthoven@gmail.com>
2022-06-12 00:20:01 +0100

    correct typo in subscriber migration

--- database/migrations/2022_06_08_212502_create_subscribers_table.php
+++ database/migrations/2022_06_08_212502_create_subscribers_table.php
@@ -15,9 +15,9 @@ return new class extends Migration {
     {
         Schema::create('subscribers', function (Blueprint $table) {
             $table->id();
-            $table->string('email');
+            $table->string('email')->unique();
             $table->string('name');
-            $table->enum('status', [
+            $table->enum('state', [
                 State::Active->value,
                 State::Bounced->value,
                 State::Unconfirmed->value,

48a4c023b7b76671e41328eb03be0e5d86e6ed7a
Victor Abbah Nkoms <veekthoven@gmail.com>
2022-06-12 00:21:51 +0100

    Add API resource classes for subscribers and fields

---
+++ app/Http/Resources/SubscriberResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SubscriberResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            "id" => $this->id,
+            "email" => $this->email,
+            "name" => $this->name,
+            "state" => $this->state,
+            "fields" => $this->fields->value,
+            "created_at" => $this->created_at,
+            "updated_at" => $this->updated_at
+        ];
+    }
+}
